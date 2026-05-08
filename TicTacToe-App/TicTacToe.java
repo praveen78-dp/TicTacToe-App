8b1fc5ac8e7540666e2fd060b7c4ebb90d382540
Praveen <pm9693@srmist.edu.in>
2026-05-08 10:41:08 +0530

    feat: complete UC2 toss and symbol assignment

--- TicTacToe-App/TicTacToe.java
+++ TicTacToe-App/TicTacToe.java
@@ -1,13 +1,62 @@
+import java.util.Random;
+
 public class TicTacToe {
-    // UC1: 2D Array for board representation
+    // UC1: Board representation
     static char[][] board = new char[3][3];
 
+    // UC2: Game State Variables
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
+
     public static void main(String[] args) {
+        // UC1 logic
         initializeBoard();
+        
+        // UC2 logic
+        tossAndAssignSymbols();
+        displayTossResult();
+        
+        // UC1 logic
         printBoard();
     }
 
-    // UC1: Initialization Logic using nested loops
+    /**
+     * UC2: Performs a random toss to decide the first player and assigns
+     * symbols (X or O) to the human and computer accordingly.
+     */
+    static void tossAndAssignSymbols() {
+        Random random = new Random();
+        // Generates 0 or 1
+        int toss = random.nextInt(2);
+
+        if (toss == 0) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
+        }
+    }
+
+    /**
+     * UC2: Displays the toss result, indicating who plays first and which
+     * symbol is assigned to each player.
+     */
+    static void displayTossResult() {
+        System.out.println("Toss result:");
+        if (isHumanTurn) {
+            System.out.println("Human plays first! Assigned Symbol: " + humanSymbol);
+        } else {
+            System.out.println("Computer plays first! Assigned Symbol: " + computerSymbol);
+        }
+        System.out.println("Computer Symbol: " + computerSymbol);
+        System.out.println("--------------------------");
+    }
+
+    // --- UC1 Methods ---
     static void initializeBoard() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -16,7 +65,6 @@ public class TicTacToe {
         }
     }
 
-    // UC1: Console Output Formatting with separators
     static void printBoard() {
         System.out.println("-------------");
         for (int i = 0; i < 3; i++) {
